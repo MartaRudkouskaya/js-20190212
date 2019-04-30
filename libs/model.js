7187cd7161151af40545634090a15f6716b2a654
Marta Rudkouskaya <stdsparrow@gmail.com>
2019-04-30 17:38:02 +0300

    update model

--- libs/model.js
+++ libs/model.js
@@ -3,9 +3,6 @@ const BASE_URL = 'https://intensive-mail-server.herokuapp.com';
 export class Model {
   request (verb, path, body) {
     const headers = {};
-    if (verb !== 'GET') {
-      headers['Content-Type'] = 'application/json';
-    }
     return fetch(`${BASE_URL}/${path}`, {
       method: verb, // GET, PUT, POST
       headers,
