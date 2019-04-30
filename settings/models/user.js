22a58f7d935196593745cafb95963a642c752f4a
Marta Rudkouskaya <stdsparrow@gmail.com>
2019-04-30 18:16:26 +0300

    First real request

--- settings/models/user.js
+++ settings/models/user.js
@@ -6,11 +6,16 @@ export class User extends Model {
    * @param {Object} form
    */
   changePassword (form) {
+    let newRequest = {};
     const newPassword = form.querySelector('#new-password').value;
     const newPasswordRepeat = form.querySelector('#new-password-repeat').value
     if (newPassword !== newPasswordRepeat || newPassword === '' || newPasswordRepeat === '') {
       return Promise.reject(new Error('Пароли не совпадают'));
+    } else {
+      newRequest['password'] = newPassword;
     }
-    return this.request('POST', 'settings', form);
+
+    console.log(JSON.stringify(newRequest));
+    return this.request('POST', 'settings', JSON.stringify(newRequest));
   }
 }
